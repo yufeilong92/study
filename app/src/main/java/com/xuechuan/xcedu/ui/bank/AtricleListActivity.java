f367204a3f0145064906f9e3a6f50e6b6322ec25
931697478@qq.com <931697478@qq.com>
2018-05-01 13:18:56 +0800

    修改图片命名,个人页面搭建

---
+++ app/src/main/java/com/xuechuan/xcedu/ui/bank/AtricleListActivity.java
@@ -0,0 +1,172 @@
+package com.xuechuan.xcedu.ui.bank;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.andview.refreshview.XRefreshView;
+import com.google.gson.Gson;
+import com.xuechuan.xcedu.R;
+import com.xuechuan.xcedu.adapter.TextBAdapter;
+import com.xuechuan.xcedu.base.BaseActivity;
+import com.xuechuan.xcedu.base.DataMessageVo;
+import com.xuechuan.xcedu.mvp.model.AtricleContentModelImpl;
+import com.xuechuan.xcedu.mvp.presenter.AtriclePresenter;
+import com.xuechuan.xcedu.mvp.view.AtricleView;
+import com.xuechuan.xcedu.utils.T;
+import com.xuechuan.xcedu.vo.SkillTextVo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Title:  AtricleTextListActivity
+ * @Package com.xuechuan.xcedu.ui.bank
+ * @Description:  章节练习首页
+ * @author: L-BackPacker
+ * @date:   2018/4/27 16:51
+ * @version V 1.0 xxxxxxxx
+ * @verdescript  版本号 修改时间  修改人 修改的概要说明
+ * @Copyright: 2018/4/27
+ */
+public class AtricleListActivity extends BaseActivity  implements AtricleView{
+
+    private RecyclerView mRlvTextContent;
+    private XRefreshView mXfvTextContent;
+    private List mArray;
+    private boolean isRefresh;
+    private Context mContext;
+    /**
+     * 科目编号
+     */
+    private static String COURSEID = "courseid";
+    private String mOid;
+    private TextBAdapter bAdapter;
+
+
+    public static Intent newInstance(Context context, String courseid) {
+        Intent intent = new Intent(context, AtricleListActivity.class);
+        intent.putExtra(COURSEID, courseid);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void initContentView(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_atricle_text);
+        if (getIntent() != null) {
+            mOid = getIntent().getStringExtra(COURSEID);
+        }
+        initView();
+        clearData();
+        setAdapter();
+        initData();
+        initRequestAll();
+    }
+
+    private void initData() {
+        AtriclePresenter atriclePresenter = new AtriclePresenter(new AtricleContentModelImpl(), this);
+        atriclePresenter.getAtricleContent(mContext,mOid);
+    }
+
+    private void initRequestAll() {
+    }
+
+    private void setAdapter() {
+        GridLayoutManager manager = new GridLayoutManager(mContext, 1);
+        manager.setOrientation(GridLayoutManager.VERTICAL);
+        bAdapter = new TextBAdapter(mContext, mArray, manager);
+        mRlvTextContent.setLayoutManager(manager);
+        mRlvTextContent.setAdapter(bAdapter);
+
+    }
+//    private void initData() {
+//        final BankService service = new BankService(mContext);
+//        service.requestChapterList(mOid, new StringCallBackView() {
+//            @Override
+//            public void onSuccess(Response<String> response) {
+//                isRefresh = false;
+//                String message = response.body().toString();
+//                L.d(message);
+//                Gson gson = new Gson();
+//                SkillTextVo vo = gson.fromJson(message, SkillTextVo.class);
+//                if (vo.getStatus().getCode() == 200) {
+//                    List<SkillTextVo.DatasBean> datas = vo.getDatas();
+//                    addListData(datas);
+//
+//                    bAdapter.notifyDataSetChanged();
+//                } else {
+//                    T.showToast(mContext, vo.getStatus().getMessage());
+//                }
+//            }
+//
+//            @Override
+//            public void onError(Response<String> response) {
+//                isRefresh = false;
+//            }
+//        });
+//    }
+
+    private void initView() {
+        mContext = this;
+        mRlvTextContent = (RecyclerView) findViewById(R.id.rlv_text_content);
+    }
+
+    private void clearData() {
+        if (mArray == null) {
+            mArray = new ArrayList();
+        } else {
+            mArray.clear();
+        }
+    }
+
+    private void addListData(List<?> list) {
+        if (list == null || list.isEmpty()) {
+            return;
+        }
+        if (mArray == null) {
+            clearData();
+        }
+        mArray.addAll(list);
+    }
+
+    /**
+     * 当前数据有几页
+     *
+     * @return
+     */
+    private int getNowPage() {
+        if (mArray == null || mArray.isEmpty())
+            return 0;
+        if (mArray.size() % DataMessageVo.CINT_PANGE_SIZE == 0)
+            return mArray.size() / DataMessageVo.CINT_PANGE_SIZE;
+        else
+            return mArray.size() / DataMessageVo.CINT_PANGE_SIZE + 1;
+    }
+
+    @Override
+    public void Success(String  content) {
+        isRefresh = false;
+        Gson gson = new Gson();
+        SkillTextVo vo = gson.fromJson(content, SkillTextVo.class);
+        if (vo.getStatus().getCode() == 200) {
+            List<SkillTextVo.DatasBean> datas = vo.getDatas();
+            addListData(datas);
+
+            bAdapter.notifyDataSetChanged();
+        } else {
+            T.showToast(mContext, vo.getStatus().getMessage());
+        }
+    }
+
+    @Override
+    public void Error(String content) {
+        isRefresh = false;
+    }
+}
